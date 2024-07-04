20cd28849f4a7ee51e1b685bc55b664565b4745d
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-04 12:33:21 +0100

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask("__name__")
+babel = Babel(app)
+
+
+class Config:
+    """Configuration for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Return the locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Return a given string"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
