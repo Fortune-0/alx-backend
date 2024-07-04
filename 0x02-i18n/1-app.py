89f3c56349db44b9d03a8bea364d5a52641e88a4
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-04 06:00:03 +0100

    - pycode validation

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -14,6 +14,7 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """Return a given string"""
