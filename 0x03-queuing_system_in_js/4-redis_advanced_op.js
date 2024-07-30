3ca93ae5fdcd8d2edeaf4e9a48d9e8a376f00361
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-30 06:42:11 +0100

    Display Hash

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,28 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', function(error) {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+// set hash key-value
+client.hset('HolbertonSchools', 'Portland', 50, print);
+client.hset('HolbertonSchools', 'Seattle', 80, print);
+client.hset('HolbertonSchools', 'New York', 20, print);
+client.hset('HolbertonSchools', 'Bogota', 20, print);
+client.hset('HolbertonSchools', 'Cali', 40, print);
+client.hset('HolbertonSchools', 'Paris', 2, print);
+
+// display all the client
+client.hgetall('HolbertonSchools', (err, reply) => {
+    if (err) {
+        console.error(`Error getting the hash: ${err}`);
+    } else {
+        console.log(reply);
+    }
+});
