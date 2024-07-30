14bda0323c05929c483f10d4792ac997d8c079cd
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-30 05:07:09 +0100

    Setting and retriving the value for school

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,39 @@
+import redis from 'redis'
+
+// Create a new Redis client
+const client = redis.createClient();
+
+// Connect to the Redis server
+client.on('connect', () => {
+    console.log('Redis client connected to the server ');
+});
+
+// Handle connection errors
+client.on('error', (err) => {
+    console.error(`Error connecting to Redis server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+    // Set the value for the specified key
+    client.set(schoolName, value, redis.print, (err, reply) => {
+        if (err) {
+            console.error(`Error setting value for ${schoolName}: ${err}`);
+        } else {
+            console.log(`Value set for ${schoolName}: ${value}`);
+        }
+    });
+}
+function displaySchoolValue(schoolName) {
+    console.get(schoolName, (err, reply) => {
+        if (err) {
+            console.error(`Error retrieving value for ${schoolName}: ${err}`);
+        } else {
+            console.log(`Value for ${schoolName}: ${reply}`);
+        }
+    })
+}
+
+// Calling the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
