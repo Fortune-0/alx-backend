f8890d9dcb826453d6a6096ca345d1aee3f656e5
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-31 03:55:30 +0100

    task 8

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,77 @@
+var kue = require('kue');
+const queue = kue.createQueue();
+
+
+const jobs = [
+    {
+      phoneNumber: '4153518780',
+      message: 'This is the code 1234 to verify your account'
+    },
+    {
+      phoneNumber: '4153518781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4153518743',
+      message: 'This is the code 4321 to verify your account'
+    },
+    {
+      phoneNumber: '4153538781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4153118782',
+      message: 'This is the code 4321 to verify your account'
+    },
+    {
+      phoneNumber: '4153718781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4159518782',
+      message: 'This is the code 4321 to verify your account'
+    },
+    {
+      phoneNumber: '4158718781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4153818782',
+      message: 'This is the code 4321 to verify your account'
+    },
+    {
+      phoneNumber: '4154318781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4151218782',
+      message: 'This is the code 4321 to verify your account'
+    }
+  ];
+
+jobs.forEach((element) => {
+    const que = queue.create('push_notification_code_2', jobs)
+    .save((err) => {
+        if (!err) {
+            console.log(`Notification job created: ${que.id}`);
+        } else {
+            console.error(`Error creating notification job: ${err}`);
+        }
+    });
+
+    que.on('complete', () => {
+        console.log(`Notification job completed: ${que.id}`);
+    });
+
+    que.on('progress', () => {
+        console.log(`Notification job progress: ${que.progress()}`);
+    })
+    
+    que.on('failed attempt', (attempt) => {
+        console.error(`Notification job failed attempt: ${que.id}, attempt: ${attempt}`);
+    });
+    
+    que.on('failed', (err) => {
+        console.error(`Notification job failed: ${que.id}, error: ${err}`);
+    });
+});
